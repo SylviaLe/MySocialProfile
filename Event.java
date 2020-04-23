939e182ca34784a3b2cb1fc13ae38d436a311cc1
SylviaLe <57645987+SylviaLe@users.noreply.github.com>
2020-04-23 20:46:50 +0700

    Update Event.java

--- Event.java
+++ Event.java
@@ -3,7 +3,7 @@ public abstract class Event
 {
     /*
     priority queue: save calendar obj, compare date, then add to queue
-    dateCompare()
+    compare()  //may need to be in another class to implement the compare() method as an obj of the Comparator interface
     addEvent()  //ask the date, ask the name, create a calendar obj then add to queue
     removeEvent() //call out when first load the profile
     display()  //when called out. Read from file then display. Format: mm dd yyyy hh mm
