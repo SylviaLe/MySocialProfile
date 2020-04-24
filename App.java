ab9081ea2f59fb103a607be06baf3b4e1c823b18
SylviaLe <57645987+SylviaLe@users.noreply.github.com>
2020-04-24 16:05:03 +0700

    Update App.java

--- App.java
+++ App.java
@@ -6,4 +6,12 @@ public abstract class App
     currentUser() //cuz the second case has more to do 
     exit()
      */
+    
+    
+    
+    //some note on event loader: 
+    /* a = min();
+    while (currentTime - min.getKey().getTime <= 0) {removeMin(), a = min()}
+    
+    
 }
