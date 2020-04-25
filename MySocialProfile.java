9319163b052f2c48bd4bbf41ee2a411d21951d76
SylviaLe <57645987+SylviaLe@users.noreply.github.com>
2020-04-25 15:54:21 +0700

    Initialize instance variables

--- MySocialProfile.java
+++ MySocialProfile.java
@@ -1,5 +1,12 @@
 public class MySocialProfile
 {
+    public String name;
+    public String email;
+    public String pass;
+    public int classYear;
+    public Event events;
+    public SinglyLinkedStack posts;
+    public ArrayList friends;
     /*createNewAcc()
     loadprofile()
     post()
