eb2ad722e2366aca0357e85f815c2b9a0e3ffd6d
Khe Le <56932982+KheVLe@users.noreply.github.com>
2020-05-09 17:07:28 +0700

    Delete DefaultComparator.java

--- dependencies/DefaultComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class DefaultComparator<E> implements Comparator<E> {
-
-   @Override
-   public int compare(E a, E b) throws ClassCastException {
-      return ((Comparable<E>) a).compareTo(b);
-   }
-}
